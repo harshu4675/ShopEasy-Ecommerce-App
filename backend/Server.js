0b9559fbdf7398d7cbcbe7b2d89f5b8235c78cda
Harsh Solanki <harshu6278@gmail.com>
2026-02-27 15:22:00 +0530

    netlify commit

--- backend/Server.js
+++ backend/Server.js
@@ -31,7 +31,7 @@ connectDB();
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://shopeasy-fashion.netlify.app",
     credentials: true, // Important for cookies
   }),
 );
